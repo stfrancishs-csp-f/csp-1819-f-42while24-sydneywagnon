a591e504ee5bf6431ac8ba481017222fb179896c
Sydney Wagnon <20wagnon@stfrancishs.org>
2018-09-18 17:30:46 +0000

    42while24 final

--- 42while24.c
+++ 42while24.c
@@ -2,21 +2,28 @@
 int main(void)
 {
     int sum=0;
-    int k;
+    int k=0;
 
-    for (k=0;k<5;k++)
+    //for (k=0;k<5;k++)
+    while (k<5)
+   {
+       //sum+=k; //use something different in "while" version
+       printf("%d\n",k);
+        sum = sum + k;
+        k++;
+    }
+
+    printf ("Sum - %d\n",sum);
+
+    //k=5;
+
+    //while (k>0)
+    for (k=10; k>=0;k--)
     {
-        sum+=k; //use something different in "while" version
+
+        //k=k-1;//use something different in "for" version
+        printf ("%2d\n", k);
     }
-
-    printf ("%d\n",sum);
-
-    k=5;
-
-    while (k>0) {
-        k=k-1; //use something different in "for" version
-        printf ("%d\n", k);
-    }
+        printf("Blastoff!");
 
 }
-
